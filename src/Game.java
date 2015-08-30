019cb8c64dee7b6bd7f0d2b143011be0e4764052
Ankit Kayastha <ankit.kayastha@duke.edu>
2015-08-30 14:41:46 -0400

    Main game file

---
+++ src/Game.java
@@ -0,0 +1,30 @@
+
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+public class Game {
+	private static String TITLE = "Mordred's Revenge";
+	private Group root; //scene graph to organize scene
+	private Scene myScene; //
+	public String getTitle() {
+		return TITLE;
+		
+	}
+	
+	public Scene init(int width, int height) {
+		//create the scene graph for organization
+		root = new Group();
+		//create the scene to see the shapes
+		myScene = new Scene(root, width, height);
+		
+		
+		
+		
+		
+		return myScene;
+		
+		
+		
+	}
+}
